261ddd780f2e76751138c9b66da9ffd5d3d1594b
Penglei Gao <48986008+PengleiGao@users.noreply.github.com>
2024-07-23 15:05:57 -0400

    Update cmd_train.py

--- cmd_train.py
+++ cmd_train.py
@@ -3,6 +3,6 @@ import os
 cmd='python main.py -b configs/train_outdoor_mamba.yaml --name mamba_outdoor'
 
 
-#cmd='python main.py -b configs/train_outdoor_mamba.yaml --name mamba_outdoor'
+#cmd='python main.py -b configs/train_indoor_mamba.yaml --name mamba_indoor'
 
 os.system(cmd)
